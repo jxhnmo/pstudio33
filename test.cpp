cefa68ba9fb9d6eb69045bba69960709baa8c5d2
Jeffrey Cheung <jcheung@tamu.edu>
2024-02-14 01:24:20 -0600

    Second push test

--- test.cpp
+++ test.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
 using namespace std;
 
+<<<<<<< Updated upstream
 int main(){
     cout <<"pulling" << endl;
     cout <<"second test" << endl;
     return 0;
+=======
+int main()
+{
+    cout << "pulling" << endl;
+
+    cout << "johns adding a line" << endl;
+
+    cout << "test 2" << endl;
+    return 0;
+
+>>>>>>> Stashed changes
 }
