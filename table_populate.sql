0a63d3b4d7d8f5037854311b15efc646da071514
goldenbushrobin <jtchau7@tamu.edu>
2024-02-20 09:13:09 -0600

    modified working shift hours

--- table_populate.sql
+++ table_populate.sql
@@ -1,10 +1,10 @@
 INSERT INTO employees (id,name,salary,shift_start,shift_end,manager)
 VALUES
-(1, 'Chee Surger', 31.5, '08:00:00', '17:00:00', true),
-(2, 'Chic Ken', 14.5, '08:00:00', '17:00:00', false),
-(3, 'Tenz Ders', 11.5, '08:00:00', '17:00:00', false),
-(4, 'Man Ger', 10.0, '08:00:00', '17:00:00', false),
-(5, 'Han Surger', 12.0, '08:00:00', '17:00:00', false);
+(1, 'Chee Surger', 31.5, '9:30:00', '23:30:00', true),
+(2, 'Chic Ken', 14.5, '10:00:00', '23:30:00', false),
+(3, 'Tenz Ders', 11.5, '10:00:00', '23:30:00', false),
+(4, 'Man Ger', 10.0, '10:00:00', '23:30:00', false),
+(5, 'Han Surger', 12.0, '10:00:00', '23:30:00', false);
 
 COPY inventory_items FROM 'inventory_items.csv' CSV HEADER
 COPY menu_items FROM 'menu_items.csv' CSV HEADER
