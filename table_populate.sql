b571fbb635b4d5368fff71fbd5c0b9fb71de6779
goldenbushrobin <jtchau7@tamu.edu>
2024-02-20 07:23:23 -0600

    add rest of table_populate script

--- table_populate.sql
+++ table_populate.sql
@@ -5,3 +5,12 @@ VALUES
 (3, 'Tenz Ders', 11.5, '08:00:00', '17:00:00', false),
 (4, 'Man Ger', 10.0, '08:00:00', '17:00:00', false),
 (5, 'Han Surger', 12.0, '08:00:00', '17:00:00', false);
+
+COPY inventory_items FROM 'inventory_items.csv' CSV HEADER
+COPY menu_items FROM 'menu_items.csv' CSV HEADER
+COPY ingredients FROM 'ingredients.csv' CSV HEADER
+COPY inventory_transactions FROM 'inventory_transactions.csv' CSV HEADER
+COPY inventory_item_orders FROM 'inventory_item_orders.csv' CSV HEADER
+COPY sales_transactions FROM 'sales_transactions.csv' CSV HEADER
+COPY sales_items FROM 'sales_items.csv' CSV HEADER
+COPY customizations FROM 'customizations.csv' CSV HEADER
