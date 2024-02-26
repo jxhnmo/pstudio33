e41f1674d1f531581df77db339a026de2d8fdf70
thomasbolf <thomasbolf1@tamu.edu>
2024-02-26 14:48:41 -0600

    Essentially copied what was in the activity and removed everything until i had no more errrors

---
+++ RevsJava/JavaFunctions/src/DbSetup.java
@@ -0,0 +1,8 @@
+public  final class DbSetup {
+    public static final String name = "csce315_903_03_db";
+    public static final String user = "csce315_903_03_user";
+    public static final String password = "PIJ5JlKq";
+    public static final String url = String.format("jdbc:postgresql://csce-315-db.engr.tamu.edu/%s", name);
+    
+  }//end class
+  
