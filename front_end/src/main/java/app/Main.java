13e079e07e2db50962a30a2e25174e0427d87526
John Mo <peikaimo@gmail.com>
2024-02-25 18:23:38 -0600

    resize menu

--- front_end/src/main/java/app/Main.java
+++ front_end/src/main/java/app/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
     public static void navigateTo(String fxml) {
         try {
             Parent root = FXMLLoader.load(Main.class.getResource(fxml + ".fxml"));
-            Scene scene = new Scene(root, 1600, 1200); // Adjusted size of screen
+            Scene scene = new Scene(root, 1200, 900); // Adjusted size of screen
             primaryStage.setScene(scene);
         } catch (Exception e) {
             e.printStackTrace();
