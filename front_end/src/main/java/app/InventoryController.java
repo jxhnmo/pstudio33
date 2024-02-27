50a4df9be7253ae304e10ef80269c9a2ee89a930
etvan13 <123894405+etvan13@users.noreply.github.com>
2024-02-27 11:40:08 -0600

    Fixed sign off

--- front_end/src/main/java/app/InventoryController.java
+++ front_end/src/main/java/app/InventoryController.java
@@ -1,5 +1,6 @@
 package app;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.fxml.FXML;
@@ -12,8 +13,11 @@ public class InventoryController {
     private void handleSignOff(ActionEvent event) {
         System.out.println("Signed off");
         // Any additional sign-off logic can go here
-    }
 
+        // Quit the JavaFX application
+        Platform.exit();
+    }
+    
     public void goToMenu(ActionEvent event) {
         app.Main.navigateTo("Menu");
     }
