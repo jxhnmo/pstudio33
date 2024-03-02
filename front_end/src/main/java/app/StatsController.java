9934eb6bc251b213cd8d251ec9f9a0b304dddbd4
Jeffrey Cheung <jcheung@tamu.edu>
2024-03-02 17:44:29 -0600

    Cleaned up line chart element

--- front_end/src/main/java/app/StatsController.java
+++ front_end/src/main/java/app/StatsController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 
@@ -17,18 +18,34 @@ public class StatsController {
     private LineChart<String, Number> lineChart;
 
     @FXML
-    public void initializeCharts() {
+    private BarChart<String, Number> salesReportChart;
+
+    @FXML
+    public void initializeLineChart() {
         XYChart.Series<String, Number> series = new XYChart.Series<>();
         series.setName("Monthly Sales");
     
-        // Sample data, replace with actual data from your application
+        // Sample data
         series.getData().add(new XYChart.Data<>("Jan", 200));
         series.getData().add(new XYChart.Data<>("Feb", 300));
         series.getData().add(new XYChart.Data<>("Mar", 250));
     
         lineChart.getData().add(series);
     }
+
+    @FXML
+    public void initializeBarChart() {
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("Sales Report");
+
+        // Sample data
+        series.getData().add(new XYChart.Data<>("Product A", 150));
+        series.getData().add(new XYChart.Data<>("Product B", 200));
+        series.getData().add(new XYChart.Data<>("Product C", 100));
     
+        salesReportChart.getData().add(series);
+    }
+
     @FXML
     private void handleSignOff(ActionEvent event) {
         System.out.println("Signed off");
