6f53c2256826076a7be6b9d6ad7b556072b0b614
goldenbushrobin <jtchau7@tamu.edu>
2024-03-05 23:27:15 -0600

    added max_stock to inventory_items

--- table_populate.sql
+++ table_populate.sql
@@ -7,10 +7,11 @@ VALUES
 (5, 'Han Surger', 12.0, '10:00:00', '23:30:00', false);
 
 \copy inventory_items FROM 'inventory_items.csv' CSV HEADER;
+ALTER TABLE inventory_items ADD COLUMN max_stock INT;
+UPDATE inventory_items SET max_stock = ROUND(stock/50.0)*50;
 \copy menu_items FROM 'menu_items.csv' CSV HEADER;
 \copy ingredients FROM 'ingredients.csv' CSV HEADER;
 \copy inventory_transactions FROM 'inventory_transactions.csv' CSV HEADER;
 \copy inventory_item_orders FROM 'inventory_item_orders.csv' CSV HEADER;
 \copy sales_transactions FROM 'sales_transactions.csv' CSV HEADER;
 \copy sales_items FROM 'sales_items.csv' CSV HEADER;
--- \copy customizations FROM 'customizations.csv' CSV HEADER;
